ba35d3458e9cb1aff401364c516b73ed8758c72f
thijsjuuhh <thijsalberts@home.nl>
2018-11-28 10:31:52 +0100

    Added Window and Main Class

---
+++ PrinterSoftware/code/com/thijsjuuhh/PrintSoftware/Graphics/Window.java
@@ -0,0 +1,24 @@
+package com.thijsjuuhh.PrintSoftware.Graphics;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Window extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+	public Window(int width, int height, String title, boolean resizable, boolean undecorated, boolean visible, boolean full_screen) {
+		setTitle(title);
+		setMinimumSize(new Dimension(width, height));
+		if(full_screen) 
+			setExtendedState(MAXIMIZED_BOTH);
+		setResizable(resizable);
+		setLocationRelativeTo(null);
+		setUndecorated(undecorated);
+		setVisible(visible);
+	}
+	
+	public Window(int width, int height, String title, boolean full_screen) {
+		this(width, height, title, true, false, true, full_screen);
+	}
+}
---
+++ PrinterSoftware/code/com/thijsjuuhh/PrintSoftware/Main.java
@@ -0,0 +1,11 @@
+package com.thijsjuuhh.PrintSoftware;
+
+import com.thijsjuuhh.PrintSoftware.Graphics.Window;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Window window = new Window(1200, 600, "Printer", true);
+	}
+	
+}
